430b1e7942e307b8ab862e142d7f4048f43a6226
Dina <example@gmail.com>
2022-04-26 21:04:02 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log(1);
+
+let num = 5;
+const leftBorder = 1;
+
+num = 10;
+console.log(num);
+
+const obj = {
+	name: 'Dina',
+	age: 20,
+	isMarried: false
+};
+console.log(obj.name);
+
+//const result = confirm("are you here?");
+const result = +prompt("lalala", "lalala");
+console.log(result);
